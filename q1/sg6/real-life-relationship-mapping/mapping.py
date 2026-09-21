ec7615d05770020ed25d9d87ee095583f77550ad
MahnAngeloNatividad <mcnatividad@zrc.pshs.edu.ph>
2026-09-21 20:28:20 +0700

    mapping

---
+++ q1/sg6/real-life-relationship-mapping/mapping.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
